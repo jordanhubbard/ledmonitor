724536201f7ffe7507ddf7b3e524a47d57bad9fa
Jordan Hubbard <jordanhubbard@gmail.com>
2022-10-16 14:29:01 -0700

    Make sure all of the LEDs are off before doing a test.

--- test_tristate_led.py
+++ test_tristate_led.py
@@ -8,6 +8,12 @@ from ledcontrol import led_colors, led_color, led_color_blink
 
 def test_leds(sleep_interval):
     """Run through all LED colors and turn each on and off, then blink it"""
+
+    # First make sure that all of the LEDs are off
+    led_color("red", False)
+    led_color("green", False)
+    led_color("blue", False)
+
     colors = led_colors()
     while True:
         for col in colors:
